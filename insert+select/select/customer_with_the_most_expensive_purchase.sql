5f10129d996e730efc1f9fd7e9d677f845c968af
foramer <vlad09fedko@gmail.com>
2026-09-05 18:11:34 +0300

    Redo last task with select

--- insert+select/select/customer_with_the_most_expensive_purchase.sql
+++ insert+select/select/customer_with_the_most_expensive_purchase.sql
@@ -1,6 +1,8 @@
 SELECT
 customers.full_name,
-orders.amount
+items.price
 FROM customers
-LEFT JOIN orders ON orders.customer_id = customers.id
-WHERE amount = (SELECT MAX(amount) FROM orders);
+LEFT JOIN orders ON customers.id = orders.customer_id
+LEFT JOIN item_order ON item_order.order_id = orders.id 
+LEFT JOIN items ON item_order.item_id = items.id 
+WHERE price = (SELECT MAX(price) FROM items);
